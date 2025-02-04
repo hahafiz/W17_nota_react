d66e77f5bce8768872aac90316d496118700bf52
hahafiz <hafizqamarul19@gmail.com>
2025-02-04 21:14:11 +0800

    register API call

---
+++ src/api/auth.js
@@ -0,0 +1,18 @@
+export const register = async ({ username, email, password }) => {
+  try {
+    // call API secara optimistik (assume takde masalah)
+    const response = await fetch({
+      url: `${import.meta.env.API_URL}/api/auth/register`,
+      method: "POST",
+      body: {
+        username: username,
+        email: email,
+        password: password,
+      },
+    });
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
